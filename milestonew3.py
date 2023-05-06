159ca1c7999b95659ff2f9fd6559cf99ce06db1b
vivienneamoh12 <amu23002@byui.edu>
2023-05-06 09:55:43 +0200

    first commit

---
+++ milestonew3.py
@@ -0,0 +1,21 @@
+childs_meal=input('What is the price of a childs meal?')
+adults_meal=input('What is the price of an adults meal?')
+number_of_children=input('How many children are there?')
+number_of_adults=input('How many adults are there?')
+sales_tax=input('What is the sales tax rate?')
+
+childs_meal=(float)(4.50)
+adults_meal=(9.00)
+num_of_children=(4)
+num_of_adults=(2)
+sales_tax_rate=(6)
+
+print(adults_meal*num_of_adults+childs_meal*num_of_children)
+
+Subtotal=input('What is the subtotal?')
+Sales_tax=input('What is the sales tax?')
+Total=input('What is the total?')
+
+payment_amount=input('What is the payment method?')
+Change=input('Type the change')
+
---
+++ wordgames.py
@@ -0,0 +1,21 @@
+adjective: ('angry')
+colour: ('Red')
+verb: ('cough')
+exclamation: ('leaped')
+verb: ('ran')
+animal: ('elephant')
+adjective: ('cried')
+exclamation: ('oohh')
+Verb: ('slipped')
+animal: ('Dog')
+colour : ('pink')
+adjective: ('sang')
+exclamation: ('Horrayy')
+adjective: ('Smiled')
+
+('The other day, I was really in trouble.) 'It all started when I saw a very
+[angry] [elephant] [ran] down the hallway. '[oohh] I yelled.' ('But all
+I could think to do was to [cough] over and over'') Miraculously,
+that caused it to stop, but not before it tried to [slipped]
+right in front of my family.')('My dad was [Red] in the face as the [Dog] [sang].') ('We all [Smiled] at the [pink] room now filled with the 
+[elephant] ('We all screamed [Horrayy!].') ('Later on that day we all [leaped] for joy')
